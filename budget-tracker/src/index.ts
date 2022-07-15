f3a3ff8d2613260ce18de89173bd6def8fc2ba60
Ronen Konfortes <konfortes@gmail.com>
2022-07-15 22:00:17 +0300

    Normalize txns and output to csv (#5)

--- budget-tracker/src/index.ts
+++ budget-tracker/src/index.ts
@@ -1,4 +1,7 @@
-import { importTransactions } from './scraping'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as fs from 'fs'
+import { stringify } from 'csv-stringify/sync'
+import { getScrapers } from './scraping'
 ;(async () => {
     const cliArgs = process.argv.slice(2)
 
@@ -11,6 +14,18 @@ import { importTransactions } from './scraping'
     }
     const res = await importTransactions(fromDate)
 
-    console.log(`Got ${res.length} transactions`)
-    console.log(res)
+    const output = stringify(res, { header: true })
+    fs.writeFileSync(`./transactions_${fromDate}.csv`, output)
 })()
+
+export async function importTransactions(startDate: Date | string) {
+    if (!(startDate instanceof Date)) {
+        startDate = new Date(startDate)
+    }
+
+    const [beinleumiScraper, maxScraper] = getScrapers(startDate)
+
+    const [beinleumiTransactions, maxTransactions] = await Promise.all([beinleumiScraper.scrape(), maxScraper.scrape()])
+
+    return beinleumiTransactions.concat(maxTransactions)
+}
--- budget-tracker/src/scraping/index.ts
+++ budget-tracker/src/scraping/index.ts
@@ -2,19 +2,7 @@ import { BeinleumiScraper } from './scrapers/beinleumi'
 import { MaxScraper } from './scrapers/max'
 import { TransactionsScraper } from './scrapers/scraper'
 
-export async function importTransactions(startDate: Date | string) {
-    if (!(startDate instanceof Date)) {
-        startDate = new Date(startDate)
-    }
-
-    const [beinleumiScraper, maxScraper] = getScrapers(startDate)
-
-    const [beinleumiTransactions, maxTransactions] = await Promise.all([beinleumiScraper.scrape(), maxScraper.scrape()])
-
-    return beinleumiTransactions.concat(maxTransactions)
-}
-
-function getScrapers(startDate: Date): TransactionsScraper[] {
+export function getScrapers(startDate: Date): TransactionsScraper[] {
     const commonScraperOptions = {
         startDate: new Date(startDate),
         combineInstallments: false,
